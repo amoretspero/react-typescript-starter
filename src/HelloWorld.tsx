dcef7f06a573bdceab04f8a0ece801f5d02bb6de
Jiung Hahm <amoretspero@gmail.com>
2020-04-15 01:08:03 +0900

    Added comments.

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,5 +1,21 @@
 import React from "react";
 
+/**
+ * The smallest example.
+ * 
+ * Below function will return <h1> element with "Hello, world!" inside it.
+ * 
+ * When rendered with
+ * ```javascript
+ * ReactDOM.render(
+ *     <HelloWorld />,
+ *     document.getElementById("root")
+ * );
+ * ```
+ * it will display a heading saying "Hello, world!" on the page.
+ * 
+ * Above rendering code can be found in `index.tsx`.
+ */
 export function HelloWorld() {
     return <h1>Hello, world!</h1>;
 }
--- src/IntroducingTsx.tsx
+++ src/IntroducingTsx.tsx
@@ -1,30 +1,69 @@
 import React from "react";
 
-// const element = <h1>Hello, world!</h1>;
+/**
+ * This syntax is called JSX. Of course, when in typescript, called TSX.
+ * 
+ * JSX(TSX) is a syntax extension to Javascript(Typescript)
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const tsxElement = <h1>Hello, world!</h1>;
 
-// const name = "Josh Perez";
-// const nameElement = <h1>Hello, {name}</h1>;
+const name = "Josh Perez";
 
+/**
+ * String typed variable `name` can be used inside of TSX, by wrapping it in curly braces.
+ * 
+ * Any valid typescript expression can be placed inside of curly braces in TSX.
+ * For example, `2 + 2`, `user.firstName`, `formatName(user)` are all valid expressions that can be put inside of TSX.
+ * 
+ * You can think that given expression is first evaluated, then take its stringified form when takes place in TSX.
+ * 
+ * TSX can prevent injection attacks, by used with curly braces.
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const nameElement = <h1>Hello, {name}</h1>;
+
+/**
+ * User interface.
+ */
 interface IUser {
     firstName: string;
     lastName: string;
 }
 
+/**
+ * Formats user's name to string.
+ * @param user User to format name.
+ */
 function formatName(user: IUser): string {
     return `${user.firstName} ${user.lastName}`;
 }
 
+/**
+ * Test user.
+ */
 const user: IUser = {
     firstName: "Harper",
     lastName: "Perez"
 };
 
+/**
+ * Element that result of calling typescript function `formatName` will be embedded in TSX.
+ */
 const element = (
     <h1>
         Hello, {formatName(user)}
     </h1>
 );
 
+/**
+ * A function that returns greeting for given user, as TSX.
+ * 
+ * We can think TSX as an typescript expression.
+ * TSX can be used inside of `if` statements and `for` loops, assigned to variables,
+ * accepted as arguments and returned.
+ * @param user User to greet.
+ */
 function getGreeting(user?: IUser) {
     if (user) {
         return <h3>Hello, {formatName(user)}!</h3>;
@@ -32,8 +71,20 @@ function getGreeting(user?: IUser) {
     return <h3>Hello, Stranger.</h3>;
 }
 
+/**
+ * <img> element with `src` attribute specified.
+ * 
+ * One can use quotes to specify string literals as attributes.(ex: `<div tabIndex="0"></div>`)
+ * 
+ * One also can use curly braces to embed a Typescript expression in an attribute.(ex: See below.)
+ */
 const imgElement = <img src={"https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG"} />;
 
+/**
+ * TSX wrap-up element.
+ * 
+ * TSX tags may contain children, as `div` element in below code does.
+ */
 export function IntroducingTsx() {
     return (
         <div>
@@ -46,3 +97,23 @@ export function IntroducingTsx() {
         </div>
     );
 }
+
+/**
+ * Below two element is exactly the same.
+ * 
+ * `elementWithTsx` is created with TSX, where `elementWithCreateElement` is created with explicit call to `createElement` function from react.
+ * 
+ * TSX is recommended for some reasons. For detailed explanation, please see https://reactjs.org/docs/introducing-jsx.html#why-jsx
+ */
+
+/**
+ * Element created with TSX.
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const elementWithTsx = <h1 className="greeting">Hello, world!</h1>;
+
+/**
+ * Element created with `createElement` function.
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const elementWithCreateElement = React.createElement("h1", { className: "greeting" }, "Hello, world!");
